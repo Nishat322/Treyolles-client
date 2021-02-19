c604702495949705544927be781730f51dca335a
Nishat Hoque <hoquen0397@gmail.com>
2021-02-19 14:04:15 -0500

    all components complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-function App(){
+import List from './List'
+import store from './store.js'
+
+import './App.css'
+
+function App(props){
   return (
     <main className = 'App'>
-
+      <header class="App-header">
+        <h1>Trelloyes!</h1>
+      </header>
+      <div class="App-list">
+        {props.store.lists.map(list => (
+          <List
+           key = {list.id}
+           header = {list.header}
+           cards = {list.cardIds.map(id => store.allCards[id])}
+          />
+        ))}
+      </div>
     </main>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import STORE from './store'
 import App from './App'
 
 import './index.css'
 
 ReactDOM.render(
-  <App />, document.getElementById('root')
+  <App store = {STORE} />, document.getElementById('root')
 );
